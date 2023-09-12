25961e2dea441f34546150c788c16f8cb6b1d82d
psannikov <psannikov87@gmail.com>
2023-09-12 08:40:07 +0400

    first commit hw04-gc

---
+++ hw04-gc/src/main/java/ru/otus/pro/psannikov/calculator/Data.java
@@ -0,0 +1,13 @@
+package ru.otus.pro.psannikov.calculator;
+
+public class Data {
+    private final Integer value;
+
+    public Data(Integer value) {
+        this.value = value;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+}
