a387b4c066c6d4f973edb03d6c72f79ade3de825
psannikov <psannikov87@gmail.com>
2023-09-15 22:19:52 +0400

    fix spy test

--- hw02-unittest/src/test/java/ru/otus/pro/psannikov/cashmachine/machine/service/CashMachineServiceTest.java
+++ hw02-unittest/src/test/java/ru/otus/pro/psannikov/cashmachine/machine/service/CashMachineServiceTest.java
@@ -6,10 +6,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import ru.otus.pro.psannikov.cashmachine.TestUtil;
+import ru.otus.pro.psannikov.cashmachine.bank.dao.AccountDao;
 import ru.otus.pro.psannikov.cashmachine.bank.dao.CardsDao;
 import ru.otus.pro.psannikov.cashmachine.bank.data.Card;
 import ru.otus.pro.psannikov.cashmachine.bank.service.AccountService;
 import ru.otus.pro.psannikov.cashmachine.bank.service.CardService;
+import ru.otus.pro.psannikov.cashmachine.bank.service.impl.AccountServiceImpl;
 import ru.otus.pro.psannikov.cashmachine.bank.service.impl.CardServiceImpl;
 import ru.otus.pro.psannikov.cashmachine.machine.data.CashMachine;
 import ru.otus.pro.psannikov.cashmachine.machine.data.MoneyBox;
@@ -43,25 +45,27 @@ class CashMachineServiceTest {
 
     private CashMachine cashMachine = new CashMachine(moneyBox);
     String cardNum = "1234567890";
+    String cardNumForError = "0123456789";
     String pin = "1234";
     String newPin = "4321";
     BigDecimal amount = new BigDecimal(1000);
     BigDecimal addMoney = new BigDecimal(100+500+1000+5000);
     BigDecimal subMoney = new BigDecimal(1100);
+    Card card;
+    Card cardForError;
 
 
     @BeforeEach
     void init() {
-        Card card = new Card(1L, cardNum, 1L, TestUtil.getHash(pin));
-        when(cardsDao.getCardByNumber(anyString())).thenReturn(card);
-//        when(cardService.getBalance(cardNum, pin)).thenReturn(amount);
-//        when(cardService.getMoney(cardNum, pin,amount)).thenReturn(amount);
-        cashMachineService = new CashMachineServiceImpl(cardService, accountService, moneyBoxService);
+        card = new Card(1L, cardNum, 1L, TestUtil.getHash(pin));
+        cardForError = new Card(2L, cardNum, 2L, TestUtil.getHash(newPin));
+        cashMachineService = spy(new CashMachineServiceImpl(cardService, accountService, moneyBoxService));
     }
 
 
     @Test
     void getMoney() {
+        when(cardsDao.getCardByNumber(cardNum)).thenReturn(card);
         List<Integer> listOfNotesAfterGetMoney = List.of(0,1,0,1);
         CashMachine cashMachineSpy = spy(cashMachine);
         List<Integer> result = cashMachineService.getMoney(cashMachineSpy,cardNum,pin,subMoney);
@@ -70,6 +74,7 @@ class CashMachineServiceTest {
 
     @Test
     void putMoney() {
+        when(cardsDao.getCardByNumber(cardNum)).thenReturn(card);
         List<Integer> notes = List.of(1, 1, 1, 1);
         when(cardService.putMoney(cardNum, pin, addMoney)).thenReturn(amount.add(addMoney));
         BigDecimal result = cashMachineService.putMoney(cashMachine, cardNum, pin, notes);
@@ -78,6 +83,7 @@ class CashMachineServiceTest {
 
     @Test
     void checkBalance() {
+        when(cardsDao.getCardByNumber(cardNum)).thenReturn(card);
         when(cardService.getBalance(cardNum, pin)).thenReturn(amount);
         BigDecimal result = cashMachineService.checkBalance(cashMachine, cardNum, pin);
         assertEquals(amount, result);
@@ -85,29 +91,18 @@ class CashMachineServiceTest {
 
     @Test
     void changePin() {
-        boolean result = cashMachineService.changePin(cardNum, pin, newPin);
-        assertTrue(result);
+        when(cardsDao.getCardByNumber(cardNum)).thenReturn(card);
+        boolean result = cashMachineService.changePin(cardNum,pin,newPin);
+        verify(cashMachineService).changePin(cardNum,pin,newPin);
+        assertEquals(true, result);
+
     }
 
     @Test
     void changePinWithAnswer() {
-//        CardServiceImpl cardServiceSpy = spy(cardService);
-//        when(cardServiceSpy.cnangePin(
-//                ArgumentMatchers.anyString(),
-//                ArgumentMatchers.anyString(),
-//                ArgumentMatchers.anyString()))
-//                .thenAnswer(invocation -> {
-//                    String providedCardNum = invocation.getArgument(0);
-//                    String providedOldPin = invocation.getArgument(1);
-//                    String providedNewPin = invocation.getArgument(2);
-//                    if (providedCardNum.equals(cardNum) && providedOldPin.equals(pin)) {
-//                        return true;
-//                    } else {
-//                        return false;
-//                    }
-//                });
-        boolean result = cashMachineService.changePin(cardNum, pin, newPin);
-        assertTrue(result);
-// @TODO create change pin test using spy as implementation and mock an thenAnswer
+        when(cardsDao.getCardByNumber(cardNumForError)).thenReturn(cardForError);
+        boolean result = cashMachineService.changePin(cardNumForError,pin,newPin);
+        verify(cashMachineService).changePin(cardNumForError,pin,newPin);
+        assertEquals(false, result);
     }
 }
