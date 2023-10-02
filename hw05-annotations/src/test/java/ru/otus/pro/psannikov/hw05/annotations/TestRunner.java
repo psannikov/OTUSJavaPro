635726f62e06a9b63d596729260485ad566e8a8d
psannikov <psannikov87@gmail.com>
2023-10-02 16:36:36 +0400

    remove fields from TestRunner

--- hw05-annotations/src/test/java/ru/otus/pro/psannikov/hw05/annotations/TestRunner.java
+++ hw05-annotations/src/test/java/ru/otus/pro/psannikov/hw05/annotations/TestRunner.java
@@ -10,67 +10,61 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TestRunner {
-    private final List<Method> befoMethods = new ArrayList<>();
-    private final List<Method> afterMethods = new ArrayList<>();
-    private final List<Method> testMethods = new ArrayList<>();
-    private final List<Method> intersection = new ArrayList<>();
-    private final CalculatorTest calculatorTest = new CalculatorTest();
-    private final List<Method> successfulTest = new ArrayList<>();
-    private final List<Method> unSuccessfulTest = new ArrayList<>();
 
-    private void prepareMethodsArrays() {
-        Class<?> clazz = calculatorTest.getClass();
+    private TestData prepareMethodsArrays(TestData testData) {
+        Class<?> clazz = testData.getCalculatorTest().getClass();
         Method[] methods = clazz.getDeclaredMethods();
         for (Method method : methods) {
             Annotation[] annotations = method.getAnnotations();
             for (Annotation annotation : annotations) {
                 if (annotation instanceof BeforeMethod) {
-                    befoMethods.add(method);
+                    testData.addBefoMethods(method);
                 } else if (annotation instanceof AfterMethod) {
-                    afterMethods.add(method);
+                    testData.addAfterMethods(method);
                 } else if (annotation instanceof Test) {
-                    testMethods.add(method);
+                    testData.addTestMethods(method);
                 }
             }
         }
-        for (Method method : befoMethods) {
-            if (afterMethods.contains(method)) {
-                intersection.add(method);
+        for (Method method : testData.getBefoMethods()) {
+            if (testData.getAfterMethods().contains(method)) {
+                testData.addIntersection(method);
             }
         }
-        for (Method method : befoMethods) {
-            if (testMethods.contains(method)) {
-                intersection.add(method);
+        for (Method method : testData.getBefoMethods()) {
+            if (testData.getTestMethods().contains(method)) {
+                testData.addIntersection(method);
             }
         }
-        for (Method method : testMethods) {
-            if (afterMethods.contains(method)) {
-                intersection.add(method);
+        for (Method method : testData.getTestMethods()) {
+            if (testData.getAfterMethods().contains(method)) {
+                testData.addIntersection(method);
             }
         }
+        return testData;
     }
 
-    private boolean checkHasErrorsInAnnotations() {
-        return !intersection.isEmpty();
+    private boolean checkHasErrorsInAnnotations(TestData testData) {
+        return !testData.getIntersection().isEmpty();
     }
 
-    private void runAllTest() {
-        if (!testMethods.isEmpty()) {
-            for (Method testMethod : testMethods) {
+    private void runAllTest(TestData testData) {
+        if (!testData.getTestMethods().isEmpty()) {
+            for (Method testMethod : testData.getTestMethods()) {
                 try {
-                    for (Method beforeMethod : befoMethods) {
-                        beforeMethod.invoke(calculatorTest);
+                    for (Method beforeMethod : testData.getBefoMethods()) {
+                        beforeMethod.invoke(testData.getCalculatorTest());
                     }
-                    testMethod.invoke(calculatorTest);
-                    successfulTest.add(testMethod);
+                    testMethod.invoke(testData.getCalculatorTest());
+                    testData.addSuccessfulTest(testMethod);
                 } catch (Exception e) {
                     System.out.println(e.getMessage());
-                    unSuccessfulTest.add(testMethod);
+                    testData.addUnSuccessfulTest(testMethod);
                 }
             }
             try {
-                for (Method afterMethod : afterMethods) {
-                    afterMethod.invoke(calculatorTest);
+                for (Method afterMethod : testData.getAfterMethods()) {
+                    afterMethod.invoke(testData.getCalculatorTest());
                 }
             } catch (Exception e) {
                 System.out.println(e.getMessage());
@@ -78,17 +72,17 @@ public class TestRunner {
         }
     }
 
-    public void unitTestRunner() {
+    public void unitTestRunner(TestData testData) {
         try {
-            prepareMethodsArrays();
-            if (checkHasErrorsInAnnotations()) {
+            prepareMethodsArrays(testData);
+            if (checkHasErrorsInAnnotations(testData)) {
                 throw new IllegalArgumentException("Тест методы имеют более одной аннотации, необходимо исправить");
             }
-            runAllTest();
+            runAllTest(testData);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        System.out.println("Успешно пройденные тесты:" + successfulTest);
-        System.out.println("Неуспешно пройденные тесты:" + unSuccessfulTest);
+        System.out.println("Успешно пройденные тесты:" + testData.getSuccessfulTests());
+        System.out.println("Неуспешно пройденные тесты:" + testData.getUnSuccessfulTests());
     }
 }
--- hw05-annotations/src/test/java/ru/otus/pro/psannikov/hw05/annotations/UnitTestRunner.java
+++ hw05-annotations/src/test/java/ru/otus/pro/psannikov/hw05/annotations/UnitTestRunner.java
@@ -2,7 +2,8 @@ package ru.otus.pro.psannikov.hw05.annotations;
 
 public class UnitTestRunner {
     public static void main(String[] args) {
+        TestData testData = new TestData();
         TestRunner testRunner = new TestRunner();
-        testRunner.unitTestRunner();
+        testRunner.unitTestRunner(testData);
     }
 }
