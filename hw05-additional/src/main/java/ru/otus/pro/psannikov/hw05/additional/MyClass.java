b358e5eb494e68139bdbc80285a4c5738e0ff1c6
psannikov <psannikov87@gmail.com>
2023-10-18 20:50:25 +0400

    first commit

---
+++ hw05-additional/src/main/java/ru/otus/pro/psannikov/hw05/additional/MyClass.java
@@ -0,0 +1,8 @@
+package ru.otus.pro.psannikov.hw05.additional;
+
+@CustomToString
+public class MyClass {
+    private String text;
+    private Integer number;
+    private Long numberTwo;
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
 rootProject.name = "otus-pro-homeworks"
-
+include("cw14-structural-patterns")
