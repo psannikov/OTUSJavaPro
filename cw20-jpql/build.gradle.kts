e6f913842443f33012be33bcb1cf2d7781be8371
psannikov <p.sannikov@volga.rt.ru>
2023-11-13 15:58:58 +0400

    finish

--- cw20-jpql/build.gradle.kts
+++ cw20-jpql/build.gradle.kts
@@ -10,8 +10,22 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.projectlombok:lombok:1.18.24")
+    annotationProcessor("org.projectlombok:lombok:1.18.24")
+
+    implementation("ch.qos.logback:logback-classic:1.4.5")
+    implementation("org.hibernate.orm:hibernate-core:6.3.1.Final")
+    implementation("org.flywaydb:flyway-core:9.22.0")
+
+    implementation("org.postgresql:postgresql:42.6.0")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.1")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.7.0")
+
+    testImplementation("org.testcontainers:junit-jupiter:1.19.1")
+    testImplementation("org.testcontainers:postgresql:1.19.1")
 }
 
 tasks.test {
