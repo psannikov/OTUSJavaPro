e9e732c923f81853f65ea11dfcc728523fbf79ad
psannikov <p.sannikov@volga.rt.ru>
2023-11-29 17:02:32 +0400

    modify Item, ItemsControllers add ItemsInMemoryRepository, micration sript

--- cw25-spring-boot-part1/src/main/java/ru/otus/pro/psannikov/spring/boot/part1/controllers/ItemsControllers.java
+++ cw25-spring-boot-part1/src/main/java/ru/otus/pro/psannikov/spring/boot/part1/controllers/ItemsControllers.java
@@ -1,14 +1,25 @@
 package ru.otus.pro.psannikov.spring.boot.part1.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import ru.otus.pro.psannikov.spring.boot.part1.model.Item;
+import ru.otus.pro.psannikov.spring.boot.part1.ItemsInMemoryRepository;
+import ru.otus.pro.psannikov.spring.boot.part1.entities.Item;
+
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class ItemsControllers {
+    private final ItemsInMemoryRepository itemsInMemoryRepository;
     @GetMapping("/items/{id}")
     public Item getItemById(@PathVariable Long id) {
-        return new Item(id, "Item #" + id, 100);
+        return itemsInMemoryRepository.getItemById(id);
+    }
+
+    @GetMapping("/items")
+    public List<Item> getAllItems() {
+        return itemsInMemoryRepository.getAllItems();
     }
 }
