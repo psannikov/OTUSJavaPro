7c26e41a07046e0a78692e2b39a61d3726bbf5df
psannikov <psannikov87@gmail.com>
2023-12-16 12:08:59 +0400

    first commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,4 @@
 rootProject.name = "otus-pro-homeworks"
-
+include("cw30-spring-boot-security-https")
+include("cw30-spring-boot-security-https:rest-service")
+findProject(":cw30-spring-boot-security-https:rest-service")?.name = "rest-service"
