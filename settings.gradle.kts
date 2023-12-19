91c4344bd9f54a123aa3175cd31e91363f769d1d
psannikov <psannikov87@gmail.com>
2023-12-19 23:08:38 +0400

    first commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,6 @@
 rootProject.name = "otus-pro-homeworks"
-
+include("cw31-spring-boot-security-application")
+include("cw31-spring-boot-security-application:main")
+findProject(":cw31-spring-boot-security-application:main")?.name = "main"
+include("cw31-spring-boot-security-application:approve")
+findProject(":cw31-spring-boot-security-application:approve")?.name = "approve"
