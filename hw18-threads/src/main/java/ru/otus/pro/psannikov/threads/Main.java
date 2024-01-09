0db2a1098711670c1fd7485c06ecc1a60c59e2e5
psannikov <psannikov87@gmail.com>
2024-01-10 00:03:02 +0400

    fix

--- hw18-threads/src/main/java/ru/otus/pro/psannikov/threads/Main.java
+++ hw18-threads/src/main/java/ru/otus/pro/psannikov/threads/Main.java
@@ -1,36 +1,57 @@
 package ru.otus.pro.psannikov.threads;
 
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
     private static final Lock lock = new ReentrantLock();
+    private static final Condition possibleToThread1 = lock.newCondition();
+    private static final Condition possibleToThread2 = lock.newCondition();
+    public static final String THREAD1NAME = "Thread 1";
+    public static final String THREAD2NAME = "Thread 2";
+    private static boolean firstRun = true;
+    private final static int MAXITERATION = 100;
+
 
     public static void main(String[] args) throws InterruptedException {
-        printNumbs();
-
+        threadsWork();
     }
-
-    public static void printNumbs() throws InterruptedException {
+    public static void threadsWork() throws InterruptedException {
         Runnable runnable = () -> {
-            lock.lock();
-            System.out.print(Thread.currentThread().getName() + ": ");
-            try {
-                for (int i = 1; i <= 10; i++) {
-                    System.out.print(i + " ");
+            int iteration = 0;
+            Printer printer1 = new Printer();
+            Printer printer2 = new Printer();
+            while (iteration < MAXITERATION) {
+                iteration++;
+                lock.lock();
+                try {
+                    if (firstRun) {
+                        firstRun = false;
+                        possibleToThread1.signal();
+                        possibleToThread2.await();
+                    }
+                    System.out.print(Thread.currentThread().getName() + " ");
+                    if (Thread.currentThread().getName() == THREAD1NAME) {
+                        printer1.printNumbers();
+                        possibleToThread2.signal();
+                        possibleToThread1.await();
+                    } else if (Thread.currentThread().getName() == THREAD2NAME) {
+                        printer2.printNumbers();
+                        possibleToThread1.signal();
+                        possibleToThread2.await();
+                    }
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    lock.unlock();
                 }
-                for (int i = 9; i >= 1; i--) {
-                    System.out.print(i + " ");
-                }
-                System.out.println();
-            } finally {
-                lock.unlock();
             }
         };
         Thread thread1 = new Thread(runnable);
-        thread1.setName("Поток 1");
+        thread1.setName(THREAD1NAME);
         Thread thread2 = new Thread(runnable);
-        thread2.setName("Поток 2");
+        thread2.setName(THREAD2NAME);
         thread1.start();
         thread2.start();
         thread1.join();
---
+++ hw18-threads/src/main/java/ru/otus/pro/psannikov/threads/Printer.java
@@ -0,0 +1,22 @@
+package ru.otus.pro.psannikov.threads;
+
+public class Printer {
+    private int cnt=0;
+    private final int MINVALUE = 0;
+    private final int MAXVALUE = 10;
+    private int incr = 1;
+    public void printNumbers() {
+             if (cnt>MINVALUE & cnt<MAXVALUE) {
+                 System.out.println(cnt);
+             } else {
+                 if (cnt==0) {
+                     System.out.println(cnt);
+                     incr = 1;
+                 } else {
+                     System.out.println(cnt);
+                     incr = -1;
+                 }
+             }
+             cnt = cnt + incr;
+    }
+}
